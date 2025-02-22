fd7e0c1c09f264592bd3184d445e4024b25ec5df
momosau <msdhhgg6@gmail.com>
2025-02-22 17:42:02 +0300

    connection

--- RootedBack/Models/RootedDBContext.cs
+++ RootedBack/Models/RootedDBContext.cs
@@ -37,7 +37,7 @@ public partial class RootedDBContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LAPTOP-JAJ6P55J\\SQLEXPRESS01;Database=RootedDB;Trusted_Connection=True;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("DefaultConnection");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- RootedBack/Program.cs
+++ RootedBack/Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using RootedBack.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<RootedDBContext>(options =>
+    options.UseSqlServer(connectionString));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//builder.Services.AddHttpClient<ProductService>();
+
 
 var app = builder.Build();
 
