672872c22a6c6e72f784f16beb154bfc3d5fde02
momosau <msdhhgg6@gmail.com>
2025-02-22 22:29:45 +0300

    fix con string

--- RootedBack/Models/RootedDBContext.cs
+++ RootedBack/Models/RootedDBContext.cs
@@ -36,7 +36,6 @@ public partial class RootedDBContext : DbContext
     public virtual DbSet<Specification> Specifications { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("DefaultConnection");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
--- RootedBack/Program.cs
+++ RootedBack/Program.cs
@@ -1,16 +1,33 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using RootedBack.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())  // Ensure the correct base path
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+Console.WriteLine("Checking Configuration Loading...");
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (string.IsNullOrEmpty(connectionString))
+{
+    Console.WriteLine("❌ Connection string is EMPTY! Check appsettings.json.");
+}
+else
+{
+    Console.WriteLine($"✅ Connection String Loaded: {connectionString}");
+}
 builder.Services.AddDbContext<RootedDBContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 //builder.Services.AddHttpClient<ProductService>();
 
