167bbb1f5a8c6761a4ba754442fcadc3425d10b9
Raghadmj1 <raghadmojj@gmail.com>
2025-04-23 23:07:46 +0300

    x

--- RootedBack/Data/RootedDBContext.cs
+++ RootedBack/Data/RootedDBContext.cs
@@ -42,7 +42,8 @@ public partial class RootedDBContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LAPTOP-R4S9R1N0;Database=RootedDB44;Trusted_Connection=True;TrustServerCertificate=True;");
+                => optionsBuilder.UseSqlServer("Server=LAPTOP-3LKC6V7D\\SQLEXPRESS;Database=RootedDB4;Trusted_Connection=True;TrustServerCertificate=True;Connection Timeout=300;");
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.UseCollation("Arabic_CI_AS");
